f09dae1985836b99617eac6ced6db61056dd740c
William Rezende <wrassis84@gmail.com>
2022-10-01 14:57:15 -0300

    docs: creates header.sh

---
+++ header.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+###################################################################
+# Script Name   : 
+# Description   : 
+# Args          : 
+# Version       : 
+# Bash version  : 5.1.16(1)-release 
+# Author        : William Ramos de Assis Rezende
+# E-mail        : wrassis84@gmail.com
+###################################################################
