94ef9eda4bc487fca61a653e8979c2549e92a514
William Ramos de Assis Rezende <wrassis84@gmail.com>
2023-01-13 22:47:54 +0000

    refactor: add more sections on file

--- header.sh
+++ header.sh
@@ -1,11 +1,25 @@
-#!/bin/bash
+######## ENVIRONMENT ###########################################################
 
-###################################################################
-# Script Name   : 
-# Description   : 
-# Args          : 
-# Version       : 
-# Bash version  : 5.1.16(1)-release 
-# Author        : William Ramos de Assis Rezende
-# E-mail        : wrassis84@gmail.com
-###################################################################
+#!/usr/bin/env bash
+
+######## ABOUT #################################################################
+
+# Repository      : https://github.com/wrassis84/<REPO>
+# Author          : William Ramos de Assis Rezende
+# Maintainer      : William Ramos de Assis Rezende
+#
+# DataTex.sh      : List, Add and Remove users from DataTex systems.
+# Requirements    : LibTex.sh
+# Usage           : DataTex.sh [ list | add | remove ]
+
+######## TESTING ENVIRONMENT ###################################################
+
+######## TESTS/VALIDATIONS #####################################################
+
+######## VARIABLES #############################################################
+
+######## FUNCTIONS #############################################################
+
+######## MAIN CODE - START #####################################################
+
+######## MAIN CODE - END #######################################################
